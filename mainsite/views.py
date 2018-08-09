2a055d905c9847dc9490be0a2f78555770e5f268
chenxiaopangzi <306807429@qq.com>
2018-08-09 13:12:12 +0800

    Initial commit

---
+++ mainsite/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
+from django.shortcuts import redirect
+
+# Create your views here.
+
+def homepage(request):
+    tempalte = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = tempalte.render(locals())
+    return HttpResponse(html)
+
+def showpost(requestm, slug):
+    template = get_template('post.html')
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            html = template.render(locals())
+            return HttpResponse(html)
+
+    except:
+        return redirect('/')
